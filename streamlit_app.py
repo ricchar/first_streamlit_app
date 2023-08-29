9048add4adbdef8ea0ef607fecedd6fa748e7f72
ricchar <131297780+ricchar@users.noreply.github.com>
2023-08-29 14:51:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,16 @@
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 streamlit.title('My parents New Healthy Diner')
 
-streamlit.header('Breakfast menu')
+streamlit.header('Breakfast Menu')
+
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')  
-
-
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.dataframe(fruits_to_show)
-  
+streamlit.header('🍌🥭 Build Your Own  Fruit Smoothie 🥝🍇')
